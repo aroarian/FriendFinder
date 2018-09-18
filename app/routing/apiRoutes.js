295e93cc28766d325d49fa6a82e6aa2eae0085f3
aroarian <aroarian0@gmail.com>
2018-09-17 22:53:36 -0400

    clean up

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,25 +18,18 @@ router.post("/api/friends", function(req, res) {
     };
 
     var userScores = req.body.data;
-
     var totalDifference;
-
 
     for (var i = 0; i < friends.length; i++) {
       var currentFriend = friends[i];
       totalDifference = 0;
 
-      console.log(currentFriend.name);
-
-
       for (var j = 0; j < currentFriend.scores.length; j++) {
         var currentFriendScore = currentFriend.scores[j];
         var currentUserScore = userScores[j];
 
-
         totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
       }
-
 
       if (totalDifference <= bestMatch.friendDifference) {
 
